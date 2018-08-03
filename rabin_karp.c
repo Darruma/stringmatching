8e94a04fb5bf675315a1dcbe5699f2ecfe6b1e30
Darruma <30578602+Darruma@users.noreply.github.com>
2018-08-03 19:38:13 +0100

    Update rabin_karp.c

--- rabin_karp.c
+++ rabin_karp.c
@@ -1,1 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+const unsigned BASE = 257;
+const unsigned MOD = 1000000007;
+
+
+int hash(const char* input)
+{
+	long long digest = 0;
+	for (int i = 0; i < strlen(input);i++)
+	{
+		// calculates hash using rolling hash function.
+		digest = digest * BASE + input[i]; 
+		digest = digest % MOD;
+	}
+	return digest;
+
+}
+
+int rabin_karp(char* text, char* pattern)
+{
+	long long hash_pattern = hash(pattern);
+	long long hash_text = 0;
+	long long exponent = 1;
+	for (int i = 0; i < strlen(pattern);i++)
+	{
+		exponent = (exponent * BASE) % MOD;
+
+		for(int i = 0; i < strlen(text);i++)
+		{
+			hash_text = hash_text * BASE + text[i];
+			hash_text = hash_text % MOD;
+
+			if( i >= strlen(pattern))
+			{
+				hash_text -= exponent * text[i - strlen(pattern)] % MOD;
+				if(hash_text < 0)
+				{
+					hash_text = hash_text + MOD;
+				}
+			}
+			if( i >= strlen(pattern) - 1)
+			{
+				if( hash_text == hash_pattern)
+				{
+					 return i - (strlen(pattern)-1);
+				}
+			}
+		}
+
+	}	
+}
+
+int main()
+{
+	printf("%d",rabin_karp("chicken","k"));
+	return 0;
+}
+
+
+
 
